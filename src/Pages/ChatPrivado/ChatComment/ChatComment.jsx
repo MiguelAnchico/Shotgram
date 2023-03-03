80662285db96ec3d1cc21926dc2f0492de712c36
MIGUEL ANGEL ANCHICO VALENCIA <miguel.anchico@uao.edu.co>
2023-03-02 19:55:48 -0500

    Created pages

---
+++ src/Pages/ChatPrivado/ChatComment/ChatComment.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ChatComment = () => {
+	return <div>ChatComment</div>;
+};
---
+++ src/Pages/Publicacion/Comments/PostComment/PostComment.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PostComment = () => {
+	return <div>PostComment</div>;
+};
