67c70851cde4b874fef9dc4bc7bdf350c112a9a7
MIGUEL ANGEL ANCHICO VALENCIA <miguel.anchico@uao.edu.co>
2023-03-02 20:08:11 -0500

    Pages completed

---
+++ src/Pages/Configuraciones/Configuraciones.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Configuraciones = () => {
+	return <div>Configuraciones</div>;
+};
---
+++ src/Pages/Configuraciones/LinkMobile/LinkMobile.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const LinkMobile = () => {
+	return <div>LinkMobile</div>;
+};
