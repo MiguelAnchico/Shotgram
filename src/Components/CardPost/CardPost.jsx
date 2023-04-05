2bcc1ee9039bb3e493280a13fbc3f306db1315ad
mfontal <fontalmilton@gmail.com>
2023-04-05 04:09:16 -0500

    Estructura Post finalizada

--- src/Components/CardPost/CardPost.jsx
+++ src/Components/CardPost/CardPost.jsx
@@ -19,11 +19,30 @@ export const CardPost = () => {
 					className='CardPost-header_menu'
 				/>
 			</div>
-         <div className='CardPost-post'>
-            <h2>Foto</h2>
+			<div className='CardPost-post'>
+				<img src='src\Components\CardPost\img\img-post-1.jpg' alt='' />
+			</div>
+			<div className='CardPost-interaction'>
+				<div className='CardPost-interaction_icon-continer'>
+					<div className='CardPost-interaction_icon-continer_group'>
+						<img src='src\Components\assets\images\corazon.png' alt='' />
+						<img
+							src='src\Components\assets\images\comentario-menos.png'
+							alt=''
+						/>
+					</div>
+					<img src='src\Components\assets\images\estrella-border.png' alt='' />
+				</div>
+				<div className='interaccion-counter'>
+					
+						<span>34 Me gusta</span>
+						<span>102 Me gusta</span>
+					
+				</div>
+			</div>
+			<div className='CardPost-comments'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore saepe facere debitis temporibus ut. </p>
          </div>
-         <div className='CardPost-interaction'></div>
-         <div className='CardPost-comments'></div>
 		</div>
 	);
 };
