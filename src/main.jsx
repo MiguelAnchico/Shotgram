f224f257005c395f3f99f3f7b98b5cc4de5cc33a
Miguel Angel <miguel.anchico@uao.edu.co>
2023-04-04 21:39:10 -0500

    Shelton ruta agregada

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { Toaster } from 'react-hot-toast';
 
 import { Testpagemiguel } from './Devs/MiguelAnchico/Testpagemiguel';
 import { Testpagemilton } from './Devs/Milton/Testpagemilton';
+import { Testpageshelton } from './Devs/Shelton/Testpageshelton';
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 				<Routes>
 					<Route path='/miguel' element={<Testpagemiguel />} />
 					<Route path='/milton' element={<Testpagemilton />} />
+					<Route path='/shelton' element={<Testpageshelton />} />
 				</Routes>
 			</BrowserRouter>
 			<ReactQueryDevtools />
