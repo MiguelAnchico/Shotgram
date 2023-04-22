5d2966130f7a4b478d1848cd10a3c05f0d08a493
mfontal <fontalmilton@gmail.com>
2023-04-22 11:29:37 -0500

    Creacion de post

--- src/Devs/Milton/Testpagemilton.jsx
+++ src/Devs/Milton/Testpagemilton.jsx
@@ -1,10 +1,11 @@
 import { CardPost } from "../../Components/CardPost/CardPost";
 import { Buscador } from "../../Pages/Buscador/Buscador";
 import { ConfiguracionMenu } from "../../Pages/Configuraciones/ConfiguracionMenu/ConfiguracionMenu";
+import { Publicacion } from "../../Pages/Publicacion/Publicacion";
 export const Testpagemilton = () => {
 	return (
 		<div>
-			<ConfiguracionMenu />
+			<Publicacion />
 		</div>
 	);
 };
