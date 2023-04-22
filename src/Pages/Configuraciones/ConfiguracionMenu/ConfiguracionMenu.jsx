e11c1bdf475d036035eb5b72020844e867d04437
mfontal <fontalmilton@gmail.com>
2023-04-22 03:51:49 -0500

    avance en configuraciones

---
+++ src/Pages/Configuraciones/ConfiguracionMenu/ConfiguracionMenu.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ConfiguracionMenu.css';
+
+export const ConfiguracionMenu = () => {
+	return (
+		<>
+			<div className='back-icon'>
+				<img src='src/assets/images/back-arrow-icon.png' alt='' />
+			</div>
+			<div className='ConfiguracionMenu'>
+				<h2 className='ConfiguracionMenu-title'>Configuracion perfil</h2>
+				<div className='input-button'>Adios</div>
+				<button className='input-button'>hola</button>
+				<button></button>
+				<button></button>
+			</div>
+		</>
+	);
+};
--- src/Pages/Configuraciones/Configuraciones.jsx
+++ src/Pages/Configuraciones/Configuraciones.jsx
@@ -3,3 +3,4 @@ import React from 'react';
 export const Configuraciones = () => {
 	return <div>Configuraciones</div>;
 };
+ 
