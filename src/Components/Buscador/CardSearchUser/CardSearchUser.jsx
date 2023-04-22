03b5e1c81b2ca58e0a581cec5da3060699bbdc29
mfontal <fontalmilton@gmail.com>
2023-04-22 00:29:52 -0500

    actualizacion card search user

--- src/Components/Buscador/CardSearchUser/CardSearchUser.jsx
+++ src/Components/Buscador/CardSearchUser/CardSearchUser.jsx
@@ -4,11 +4,11 @@ export const CardSearchUser = ({ url, name, user }) => {
 	return (
 		<div className='CardSearchUser'>
 			<img className='CardSearchUser-img' src={url}></img>
-			<div>
-				<p>{user}</p>
-				<p>{name}</p>
+			<div className='CardSearchUser-UserDetails'>
+				<p className='CardSearchUser-UserDetails-User color-logo'>{user}</p>
+				<p className='CardSearchUser-UserDetails-Name color-black-light'>{name}</p>
 			</div>
-			<img src='' />
+			<img src='src\assets\images\flecha-angulo-derecho.png' className='CardSearchUser-icon'/>
 		</div>
 	);
 };
