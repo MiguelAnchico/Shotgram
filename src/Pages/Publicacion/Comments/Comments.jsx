d3adc8667687a53fb318c8ddc12d994b23dc5d86
mfontal <fontalmilton@gmail.com>
2023-05-15 04:16:06 -0500

    Inicio de pagina de comentarios

--- src/Pages/Publicacion/Comments/Comments.jsx
+++ src/Pages/Publicacion/Comments/Comments.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Comments.css';
 
 export const Comments = () => {
-	return <div>Comments</div>;
+	return (
+		<div className='Comments'>
+			<div className='Comments-header'>
+				<img src='src\assets\images\back-arrow-icon.png' alt='Icono de ir atras' className='Comments-header-backIcon'/>
+				<h3 className='title-lg color-logo'>Comentarios</h3>
+			</div>
+            <div></div>
+            <form>
+
+            <div className='Input'></div>       
+            </form>
+		</div>
+	);
 };
